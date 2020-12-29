2912ee60425f9d91a6ae9b8764d42ae009eeaa61
ebussiere <eddy@embtech.ca>
2020-12-28 17:13:45 -0700

    fix sp

--- cednetcoreapiapp/Controllers/OrderController.cs
+++ cednetcoreapiapp/Controllers/OrderController.cs
@@ -31,7 +31,37 @@ namespace cednetcoreapiapp.Controllers
             var food = await _foodData.GetFood();
             order.Total = order.Quantity * food.Where(x => x.Id == order.FoodId).First().Cost;
             int id = await _orderData.CreateOrder(order);
-            return Ok(id);
+            return Ok(new {Id=id});
+        }
+        [HttpGet("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> Get(int id)
+        {
+            if (id==0)
+            {
+                return BadRequest();
+            }
+
+            var order = await _orderData.GetOrderById(id);
+            if (order!=null)
+            {
+                var food = await _foodData.GetFood();
+                var output = new
+                {
+                    Order = order,
+                    ItemPurchased = food.Where(x => x.Id == order.FoodId).FirstOrDefault()?.Title
+                };
+                return Ok(output);
+            }
+            else
+            {
+                return BadRequest();
+            }
+
+
         }
 
     }
